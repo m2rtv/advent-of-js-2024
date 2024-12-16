b48add0bdd8f7ecb3bcfe66660dd499560abdfd2
Märt Villemsaar <m2rter@illlab.ee>
2024-12-16 20:07:03 +0200

    05-Character count

---
+++ src/routes/05/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	let text = $state('');
+	let charCount = $derived(text.length);
+</script>
+
+<div class="wrapper">
+	<div class="text-area-container">
+		<textarea bind:value={text}></textarea>
+		<p><span class="character-count">Character count:</span> {charCount}</p>
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.text-area-container {
+		display: grid;
+		width: 50%;
+	}
+
+	.text-area-container textarea {
+		width: 100%;
+		height: 220px;
+	}
+
+	.text-area-container p {
+		margin-top: 0.5rem;
+		text-align: right;
+		font-size: 0.875rem;
+		font-weight: 600;
+	}
+
+	.character-count {
+		font-weight: 400;
+		color: var(--color-text-muted);
+	}
+</style>
